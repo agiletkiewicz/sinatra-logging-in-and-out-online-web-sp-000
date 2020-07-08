eab22006f0d31ec9731bd9c1efed4b7f427c9d3d
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-07-08 23:36:26 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 class Helpers
   
-  def current_user(session_hash)
+  def self.current_user(session_hash)
     user = User.find_by(id: session_hash[:user_id])
     user
   end 
   
-  def is_logged_in?(session_hash)
+  def self.is_logged_in?(session_hash)
     !!current_user
   end
   
